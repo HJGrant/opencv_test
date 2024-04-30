108fcce0b31454538a77fb6b9914d8e0763e6ae9
itr <hjgrant1@gmail.com>
2024-04-30 15:23:16 +0200

    added working gst code to base_program.py

--- base_program.py
+++ base_program.py
@@ -1,19 +1,57 @@
 import cv2
+import _dbus_glib_bindings
 print(cv2.__version__)
 dispW=640
 dispH=480
 flip=2
 #Uncomment These next Two Line for Pi Camera
-#camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+#camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=RG12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 #cam= cv2.VideoCapture(camSet)
 
 #Or, if you have a WEB cam, uncomment the next line
 #(If it does not work, try setting to '1' instead of '0')
-cam=cv2.VideoCapture(0)
+#cam = cv2.VideoCapture(0)
+
+
+def __gstreamer_pipeline(
+        camera_id,
+        capture_width=1920,
+        capture_height=1080,
+        display_width=1920,
+        display_height=1080,
+        framerate=30,
+        flip_method=0,
+    ):
+    return (
+            "nvarguscamerasrc sensor-id=%d ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=True"
+            % (
+                    camera_id,
+                    capture_width,
+                    capture_height,
+                    framerate,
+                    flip_method,
+                    display_width,
+                    display_height,
+            )
+    )
+   
+cam = cv2.VideoCapture(__gstreamer_pipeline(camera_id=0, flip_method=0), cv2.CAP_GSTREAMER)
+
+if not cam.isOpened():
+ print("Cannot open camera")
+ exit()
 
 while True:
     ret, frame = cam.read()
-    cv2.imshow('nanoCam',frame)
+    cv2.imshow('FRAMOS',frame)
+    cv2.moveWindow('FRAMOS', 0, 0)
 
     if cv2.waitKey(1)==ord('q'):
         break
